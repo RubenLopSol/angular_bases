c7cf30b9691d17a5ed519e4c3721d3ab9c87a5af
Ruben Lopez <rubenlopsol86@gmail.com>
2023-09-21 19:38:05 +0200

    s5-v65

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { DbzComponent } from './pages/main-page.component'
+import { ListComponent } from './components/list/list.component';
+import { FormComponent } from './components/form/form.component';
 
 
 
 @NgModule({
   declarations: [
     DbzComponent,
+    ListComponent,
+    FormComponent,
   ],
   exports: [
     DbzComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule
   ]
 })
 export class DbzModule { }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+import { Character } from '../interfaces/character.interface';
+
+
+
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html'
 })
 
 export class DbzComponent {
+
+  public character: Character[] = [{
+    name: 'Son Goku',
+    power: 100000
+  },{
+    name:'Vegeta',
+    power: 99050
+  },{
+    name: 'Son Gohan',
+    power: 98500
+  }];
+
+  onNewCharacter(character: Character):void {
+    this.character.push(character);
+
+  }
 
 }
